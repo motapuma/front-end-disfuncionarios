0f928ace9776b0f637ea615cfb9a927d21160455
juanman82 <juan.manuel.82@gmail.com>
2015-05-11 00:55:32 -0500

    Dropdawn Funcionando de Zonas

--- testdiv/script/script.js
+++ testdiv/script/script.js
@@ -1,33 +1,16 @@
-$(function() {
+$.ajax({
+    url: 'lista.json',
+    dataType: 'json',
+    type: 'get',
+    cache: false,
+    success: function(data){
 
+        $(data.muncipios).each(function(index, value){
+            $("#myselect").append('<option value="'+ this.value +'">'+ this.name +'</option>'+'<a href="/municipios/' + this.name + '</a>')
 
+            console.log(value.name);
+        });
 
-   // prueba json
-   var json = $.ajax({
-       url: 'lista.json',
-       dataType: 'json',
-       type: 'get',
-       cache: false,
-       success: function(data){
-           $(data.muncipios).each(function(index, value){
-
-               console.log(value.name)
-
-           })
-
-
-
-
-
-       }
-   });
-
-  // template
-    $.template('municipioList', '<div><li>${name}</li></ul></div>' );
-
-
-    for (var i = 0; i <  json.length; i++) {
-        $.tmpl('municipioList', json[i]).appendTo('#muni');
 
 
     }
